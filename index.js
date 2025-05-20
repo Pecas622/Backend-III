a570a3b7efa1f8d4c5b67dd1bb99fbb81c6e40f0
Pecas622 <santiagozorrilla5@gmail.com>
2025-05-20 12:37:27 -0300

    model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cluster from "cluster";
 import { cpus } from "os";
 import dbConnect from "./src/helpers/dbConnect.helper.js";
 import indexRouter from "./src/routers/index.router.js";
+import mocksRouter from "./src/routers/mocks.router.js"; // ✅ agregado
 import winston from "./src/middlewares/winston.mid.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
@@ -16,21 +17,17 @@ import logger from "./src/helpers/logger.helper.js";
 const server = express();
 const port = process.env.PORT || 9000;
 const ready = async () => {
-    // a partir de ahora REGISTRO LOGS unicamente con winston!!!
-    //console.log("server ready on port " + port + " and mode " + argvs.mode);
     logger.INFO("server ready on port " + port + " and mode " + argvs.mode);
     logger.INFO("server ready on pid " + process.pid);
     await dbConnect(process.env.LINK_DB);
 };
 const isPrimary = cluster.isPrimary;
 if (isPrimary) {
-    /* si estoy en un proceso primario (primary) voy a crear los hijos (workers) */
     const numberOfProcess = cpus().length;
     for (let index = 1; index <= numberOfProcess; index++) {
         cluster.fork();
     }
 } else {
-    /* si estoy en un worker voy a levantar un servidor, "PRENDIENDO" un nodo del cluster */
     server.listen(port, ready);
 }
 
@@ -44,7 +41,6 @@ server.use(express.static("public"));
 
 /* router settings */
 server.use("/", indexRouter);
+server.use("/api/mocks", mocksRouter); 
 server.use(errorHandler);
 server.use(pathHandler);
-
-//console.log(process.argv);
